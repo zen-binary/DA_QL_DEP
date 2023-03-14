2b3b7bb0f73cef5be6dc58e455af9b9762f40dc7
zena <114623190+zena-git@users.noreply.github.com>
2023-03-14 15:00:03 +0700

    update hibernate config
    update hibernate config

---
+++ DA_QL_DEP/src/main/java/HibernateConfig/HibernateConfig.java
@@ -0,0 +1,43 @@
+package HibernateConfig;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateConfig {
+
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=DA_QL_DEP");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "123456");
+        properties.put(Environment.SHOW_SQL, "true");
+
+        conf.setProperties(properties);
+//        conf.addAnnotatedClass(Loai.class);
+
+        
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        getFACTORY();
+    }
+
+}
